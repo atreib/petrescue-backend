db8e0462f129e644f110647c39d52e496d831f0a
André Treib <andre.treib@eldorado.org.br>
2021-08-14 16:00:47 -0300

    Validate if latitude was provided in register pet service

--- src/presentation/services/pets/Register.spec.ts
+++ src/presentation/services/pets/Register.spec.ts
@@ -1,3 +1,4 @@
+import { MissingParameterError } from './../../../domain/errors/MissingParameterError';
 import { IHttpRequest } from '../../protocols';
 import { Register } from './Register';
 
@@ -10,11 +11,31 @@ const makeSut = (): ISutTypes => {
   return { sut }
 }
 
+const makeValidPetRegistrationRequest = (): IHttpRequest => {
+  return {
+    body: {
+      latitude: 30.0000001,
+      longitude: -51.9999919,
+    }
+  } as IHttpRequest;
+}
+
 describe('Pets Register Service Test Suite', () => {
-  it('Should return an ok', () => {
+  it('Should return an ok if provided data is valid', () => {
     const { sut } = makeSut();
-    const response = sut.handle({} as IHttpRequest)
+    const mockRequest = makeValidPetRegistrationRequest();
+    const response = sut.handle(mockRequest)
     expect(response.statusCode).toBe(200);
     expect(response.body).toEqual({ message: "ok" });
-  })
+  });
+
+  it('Should return badRequest if latitude isnt provided', () => {
+    const { sut } = makeSut();
+    const missingParam = 'latitude';
+    const mockRequest = makeValidPetRegistrationRequest();
+    delete mockRequest.body.latitude;
+    const response = sut.handle(mockRequest);
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toEqual(new MissingParameterError(missingParam))
+  });
 });
--- src/presentation/services/pets/Register.ts
+++ src/presentation/services/pets/Register.ts
@@ -1,8 +1,11 @@
 import { IHttpRequest, IHttpResponse, IHttpService } from '../../protocols';
-import { ok } from './../../utils/Responses'
+import { ok, badRequest } from './../../utils/Responses'
+import { MissingParameterError } from './../../../domain/errors/MissingParameterError';
 
 class Register implements IHttpService {
   handle(request: IHttpRequest): IHttpResponse {
+    const { latitude } = request.body;
+    if (!latitude) return badRequest(new MissingParameterError('latitude'));
     return ok({ message: "ok" });
   }
 }
