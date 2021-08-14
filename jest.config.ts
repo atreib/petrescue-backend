352b8dc22724275c40b8edd466c60b3c9934f510
André Treib <andre.treib@eldorado.org.br>
2021-08-14 15:31:15 -0300

    fix parameter in jest config file

--- jest.config.ts
+++ jest.config.ts
@@ -20,7 +20,7 @@ export default {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: '<rootDir>/src/**/*.ts',
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
