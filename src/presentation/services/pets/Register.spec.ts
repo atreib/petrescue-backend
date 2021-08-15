600a5cdd4ee6764d894878654c013363d2b5a47a
André Treib <andre.treib@eldorado.org.br>
2021-08-15 18:38:02 -0300

    Call registration service in registration request

--- src/presentation/services/pets/Register.spec.ts
+++ src/presentation/services/pets/Register.spec.ts
@@ -1,14 +1,32 @@
 import { MissingParameterError } from './../../../domain/errors/MissingParameterError';
+import { IRegister, IRegisterPetRequest } from './../../../domain/protocols/pets/IRegister';
+import { IPetModel } from './../../../domain/models/Pet';
 import { IHttpRequest } from '../../protocols';
 import { Register } from './Register';
 
 interface ISutTypes {
-  sut: Register
+  mockPetRegistration: IRegister;
+  sut: Register;
+}
+
+const makePetRegistrationServiceMock = (): IRegister => {
+  class MockPetRegistrationService implements IRegister {
+    register(pet: IRegisterPetRequest): IPetModel {
+      return {
+        id: 'mock_id',
+        insertedAt: new Date(),
+        latitude: 30.00000001,
+        longitude: -51.999999991,
+      }
+    }
+  }
+  return new MockPetRegistrationService();
 }
 
 const makeSut = (): ISutTypes => {
-  const sut = new Register()
-  return { sut }
+  const mockPetRegistration = makePetRegistrationServiceMock();
+  const sut = new Register(mockPetRegistration)
+  return { sut, mockPetRegistration }
 }
 
 const makeValidPetRegistrationRequest = (): IHttpRequest => {
@@ -48,4 +66,15 @@ describe('Pets Register Service Test Suite', () => {
     expect(response.statusCode).toBe(400);
     expect(response.body).toEqual(new MissingParameterError(missingParam))
   });
+
+  it('Should call pet registration service', () => {
+    const { sut, mockPetRegistration } = makeSut();
+    const spyRegister = jest.spyOn(mockPetRegistration, 'register');
+    const mockRequest = makeValidPetRegistrationRequest();
+    sut.handle(mockRequest);
+    expect(spyRegister).toHaveBeenCalledWith({
+      latitude: mockRequest.body.latitude,
+      longitude: mockRequest.body.longitude,
+    });
+  })
 });
--- src/presentation/services/pets/Register.ts
+++ src/presentation/services/pets/Register.ts
@@ -1,12 +1,18 @@
 import { IHttpRequest, IHttpResponse, IHttpService } from '../../protocols';
 import { ok, badRequest } from './../../utils/Responses'
 import { MissingParameterError } from './../../../domain/errors/MissingParameterError';
+import { IRegister } from './../../../domain/protocols/pets/IRegister'
 
 class Register implements IHttpService {
+  constructor(
+    private readonly registerService: IRegister
+  ) { }
+
   handle(request: IHttpRequest): IHttpResponse {
     const { latitude, longitude } = request.body;
     if (!latitude) return badRequest(new MissingParameterError('latitude'));
     if (!longitude) return badRequest(new MissingParameterError('longitude'));
+    this.registerService.register({ latitude, longitude });
     return ok({ message: "ok" });
   }
 }
